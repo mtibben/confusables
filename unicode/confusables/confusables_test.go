a47904374a43ab39638e5b09bb0c63d2d9f7629f
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-03-31 00:50:03 +0200

    add TestCompareDifferent and benchmarks

--- unicode/confusables/confusables_test.go
+++ unicode/confusables/confusables_test.go
@@ -15,11 +15,36 @@ func TestSkeleton(t *testing.T) {
 	}
 }
 
-func TestCompare(t *testing.T) {
+func TestCompareEqual(t *testing.T) {
 	s1 := "ρ⍺у𝓅𝒂ן"
 	s2 := "𝔭𝒶ỿ𝕡𝕒ℓ"
 
 	if !Confusable(s1, s2) {
 		t.Error("Skeleton strings were expected to be equal")
+	}
+}
+
+func TestCompareDifferent(t *testing.T) {
+	s1 := "Paypal"
+	s2 := "paypal"
+
+	if Confusable(s1, s2) {
+		t.Error("Skeleton strings were expected to be different")
+	}
+}
+
+func BenchmarkSkeletonNoop(b *testing.B) {
+	s := "skeleton"
+
+	for i := 0; i < b.N; i++ {
+		Skeleton(s)
+	}
+}
+
+func BenchmarkSkeleton(b *testing.B) {
+	s := "ѕ𝗄℮|е𝗍ο𝔫"
+
+	for i := 0; i < b.N; i++ {
+		Skeleton(s)
 	}
 }
