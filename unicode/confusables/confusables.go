891cb5379e379d9e9131adccc55087c71746c8e6
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-03-31 05:24:16 +0200

    heavily optimize the already-skeleton case by turning step 2 in a no-op

--- unicode/confusables/confusables.go
+++ unicode/confusables/confusables.go
@@ -21,20 +21,19 @@ func Skeleton(s string) string {
 
 	// 2. Successively mapping each source character in X to the target string
 	// according to the specified data table
-	var newS string
 	for i, w := 0, 0; i < len(s); i += w {
 		char, width := utf8.DecodeRuneInString(s[i:])
 		replacement, exists := confusablesMap[char]
 		if exists {
-			newS += replacement
+			s = s[:i] + replacement + s[i+width:]
+			w = len(replacement)
 		} else {
-			newS += s[i : i+width]
+			w = width
 		}
-		w = width
 	}
 
 	// 3. Reapplying NFD
-	s = norm.NFD.String(newS)
+	s = norm.NFD.String(s)
 
 	return s
 }
